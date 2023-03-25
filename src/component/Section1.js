c575ba3afea1cf424ebef49f1a0cb21766ec8b15
Ryan Roshandy <91825574+ryanroshandy55@users.noreply.github.com>
2023-03-25 15:27:21 +0700

    Add files via upload

---
+++ src/component/Section1.js
@@ -0,0 +1,47 @@
+// import React from 'react'
+// import '../App.css';
+// import logo from '../logo.svg';
+// import { Button, Container, Form, Nav, Navbar, NavDropdown, Accordion, ThemeProvider, Row, Col } from 'react-bootstrap';
+
+// export default function Section1() {
+//   return (
+//     <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p className=''>
+//       Edit <code>src/App.js</code> and save to reload.
+//     </p>
+//     <a
+//       className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       <Button>Learn React</Button>
+//     </a>
+//   </header>
+//   )
+// }
+
+// import logo from './logo.svg';
+// import './App.css';
+// import './component/Biodata.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Button, Container, Form, Nav, Navbar, NavDropdown, Accordion, ThemeProvider, Row, Col } from 'react-bootstrap';
+// import Biodata from './component/Biodata.js';
+// import NavbarTop from './component/NavbarTop.js';
+// import ValidateFo from './component/ValidateFo';
+
+// function App() {
+//   return (
+//     <ThemeProvider
+//     breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+//     minBreakpoint="xxs"
+//     >
+//       <div className="App"> 
+//       <ValidateFo />
+//       </div>
+//     </ThemeProvider>
+//   );
+// }
+
+// export default App;
